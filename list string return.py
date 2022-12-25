38bf077fdc50cc57482262533ef779b30b097e06
MohithaSist <101502266+MohithaSist@users.noreply.github.com>
2022-12-24 23:15:52 -0800

    Add files via upload

---
+++ list string return.py
@@ -0,0 +1,57 @@
+a=list(map(str,input().split()))
+b=input()
+l1=[]
+l2=[]
+for p in b:
+    l1.append(p)
+#print(l1)
+for i in a:
+    for j in i:
+        #l2.append(j)
+        #print(l2)
+        if p in j:
+            l2.append(i)
+print(l2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''s=input()
+len1=len(s)
+str1=''
+l=[]
+query=sorted(set(input().split()))
+for i in query:
+    if ((i[0:len1])==0):
+        print(i)'''
+            
+        
+
+
+        
+        
